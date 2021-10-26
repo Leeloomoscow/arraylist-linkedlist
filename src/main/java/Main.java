8205d02fdf988a99e733d70d3e56da5b334274a8
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-26 17:22:17 +0300

    Lesson

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Person> list = new ArrayList<>();
+
+        Person person1 = new Person("White");
+        Person person2 = new Person("Rabbit");
+
+        list.add(person1);
+        list.add(person2);
+
+        Iterator<Person> iterator = list.iterator();
+        while (iterator.hasNext()){
+            iterator.next();
+            iterator.remove();
+        }
     }
 }
+
 
